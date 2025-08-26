cd9eeb65568a925ae28f238ae0ff7db59c4c5d8d
parkashmi09 <78411265+parkashmi09@users.noreply.github.com>
2025-08-26 13:36:14 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'whale-app-3mhy8.ondigitalocean.app',
+      'seashell-app-vgu3a.ondigitalocean.app'
+    ]
+  }
 })
